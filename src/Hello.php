08035d588b7b33f7cb4b2c40583fd70fd22abdde
qiuxinshu <1044812735@qq.com>
2018-05-23 16:46:09 +0800

    the first composer library

---
+++ src/Hello.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/5/23
+ * Time: 16:41
+ */
+namespace testcomposer;
+class Hello
+{
+    public function index()
+    {
+        return "hello world";
+    }
+}
